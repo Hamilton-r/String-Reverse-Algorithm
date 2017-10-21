8293009e0cb600a5176943c1cef624a4204366d8
Richard Hamilton <hamilton.r.w@icloud.com>
2017-10-21 18:35:48 +0100

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -4,24 +4,23 @@ package algorithms;
 import java.util.Scanner;
 
 /**
- * This class will reverse a string
+ * This class will reverse an inputed string
  * @author Richard
  *
  */
 public class ReverseString {
 
 	/**
-	 * This method will take in a string, convert that to two 
-	 * char arrays, then swap the contents of one for the contents
-	 * of the other except moving in opposite directions and so
-	 * will reverse the order of the output string  
+	 * This method will take in a string, convert that to a 
+	 * char array, then swap the contents of the array from back to front
+	 * meeting in the middle.
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		//declare variables
 		Scanner input = new Scanner(System.in);
 		String inputString = "";
-		int length = 0, indexOfArrayToChange = 0, indexOfReferenceArray = 0;
+		int length = 0, indexStart = 0, indexEnd = 0;
 		
 		//get input string
 		System.out.print("Enter the string you want to reverse: ");
@@ -31,23 +30,24 @@ public class ReverseString {
 		//as index start at 0.
 		length = inputString.length()-1;
 		//set 'indexOfReferenceArray' equal to string.length-1 for use later in loop
-		indexOfReferenceArray = length;
+		indexEnd = length;
 		
 		//convert to arrays
-		char[] arrayToChange = inputString.toCharArray();
-		char[] referenceArray = inputString.toCharArray();
+		char[] array = inputString.toCharArray();
 		
-		//loop through and reverse order as long as we are still within bounds of original string length
-		while (indexOfArrayToChange<=length) {
-			//change the first letter of one array to be the same as the last letter of reference array
-			arrayToChange[indexOfArrayToChange] = referenceArray[indexOfReferenceArray];
+		//loop through and reverse order until two vars meet in middle of array
+		while (indexStart<=(length/2)) {
+			//swap array elements
+			char temp = array[indexStart];
+			array[indexStart] = array[indexEnd];
+			array[indexEnd] = temp;
 			//increment/decrement to move through arrays
-			indexOfArrayToChange++;
-			indexOfReferenceArray--;
+			indexStart++;
+			indexEnd--;
 		}
 
 		//convert back to string
-		String newString = new String(arrayToChange);
+		String newString = new String(array);
 		
 		//output result
 		System.out.println(newString);
